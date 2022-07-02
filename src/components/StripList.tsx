460bba44b8f8ed75fac1e187497f1209ca79dfac
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-07-02 15:57:36 +0900

    デザイン案

--- src/components/StripList.tsx
+++ src/components/StripList.tsx
@@ -1,20 +1,29 @@
+import { Box, ScaleFade } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import React from "react";
 import { useGetStrips } from "../database/getStrips";
 type Props = {};
 
 const StripList: React.FC<Props> = () => {
   const strips = useGetStrips();
-
   if (strips === undefined) return <p>loading...</p>;
 
   return (
-    <div className="todo_container">
-      <h2>短冊 host</h2>
+    <div className="todo_container" style={{ paddingBottom: 100 }}>
       {strips.map((strip, index) => (
-        <div className="todo" key={strip.id ? strip.id : index} style={styles.todo}>
-          <p style={styles.todoName}>{strip.name}</p>
-          <p style={styles.todoDescription}>{strip.text}</p>
-        </div>
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.6, delay: index * 0.1, type: "spring" }}
+          style={{ width: 500, alignSelf: "center" }}
+        >
+          <Box p="6" boxShadow="md" rounded="lg" m="2" bg="white">
+            <Box fontWeight="semibold" as="h4" noOfLines={1}>
+              {strip.name}
+            </Box>
+            <Box>{strip.text}</Box>
+          </Box>
+        </motion.div>
       ))}
     </div>
   );
